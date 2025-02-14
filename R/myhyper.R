4208e38fc3f21e9250db5dadc28d96bddb657a5d
Jennifer Marwitz <jennifer.a.marwitz-1@ou.edu>
2025-02-14 12:33:24 -0600

    Added myhyper() and myplot() functions

---
+++ R/myhyper.R
@@ -0,0 +1,46 @@
+#' Hypergeometric Distribution Simulation
+#'
+#' This function simulates the hypergeometric distribution by randomly sampling
+#' without replacement from a population containing a fixed number of successes.
+#'
+#' @param iter Integer. The number of iterations (simulations) to perform. Default is 100.
+#' @param N Integer. The total population size. Default is 20.
+#' @param r Integer. The number of successes in the population. Default is 12.
+#' @param n Integer. The sample size drawn in each iteration. Default is 5.
+#'
+#' @returns A named numeric vector representing the relative frequencies of the number of successes in the samples.
+#' A barplot of the proportions is also displayed.
+#' @export
+#'
+#' @examples
+#' # Run a hypergeometric simulation with default parameters
+#' myhyper()
+#'
+#' # Run a hypergeometric simulation with custom parameters
+#' myhyper(iter = 1000, n = 19, N = 20, r = 12)
+#'
+#' # Compare with the theoretical hypergeometric probability distribution
+#' dhyper(x = 0:19, m = 12, n = 8, k = 19)
+#'
+myhyper = function(iter = 100, N = 20, r = 12, n = 5) {
+  # make a matrix to hold the samples
+  # initially filled with NA's
+  sam.mat = matrix(NA, nr = n, nc = iter, byrow = TRUE)
+
+  # make a vector to hold the number of successes over the trials
+  succ = c()
+  for(i in 1:iter) {
+    # fill each column with a new sample
+    sam.mat[,i] = sample(rep(c(1,0), c(r, N-r)), n, replace = FALSE)
+    # Calculate a statistic from the sample (this case it is the sum)
+    succ[i] = sum(sam.mat[,i])
+  }
+
+  # Make a table of successes
+  succ.tab = table(factor(succ, levels = 0:n))
+  # Make a barplot of the proportions
+  barplot(succ.tab/(iter), col = rainbow(n+1), main = "HYPERGEOMETRIC simulation", xlab = "Number of successes")
+  succ.tab/iter
+}
+myhyper(iter = 1000, n= 19, N = 20, r = 12)
+dhyper(x = 0:19, m = 12, n = 8, k = 19)
---
+++ R/myplot.R
@@ -0,0 +1,50 @@
+#' Piecewise Regression for Spruce Data
+#'
+#' This function fits a piecewise linear regression model to the spruce dataset
+#' and plots the results.
+#'
+#' @param data A dataframe containing the variables "Height" and "BHDiameter".
+#' @param breakpoint The breakpoint for the piecewise function (default is 18).
+#' @return A list containing the linear model summary and the plot.
+#' @export
+myplot <- function(data, breakpoint = 18) {
+  # Ensure required columns exist
+  if (!all(c("Height", "BHDiameter") %in% colnames(data))) {
+    stop("Data must contain 'Height' and 'BHDiameter' columns.")
+  }
+
+  # Create the piecewise variable
+  data <- within(data, {
+    X <- (BHDiameter - breakpoint) * (BHDiameter > breakpoint)
+  })
+
+  # Fit the piecewise linear model
+  lmp <- lm(Height ~ BHDiameter + X, data = data)
+  tmp <- summary(lmp)
+
+  # Define function for prediction
+  myf <- function(x, coef) {
+    coef[1] + coef[2] * x + coef[3] * (x - breakpoint) * (x > breakpoint)
+  }
+
+  # Plot the data
+  plot(data$BHDiameter, data$Height, main = "Piecewise Regression",
+       xlab = "BHDiameter", ylab = "Height", pch = 16)
+
+  # Add piecewise regression curve
+  curve(myf(x, coef = tmp$coefficients[, "Estimate"]),
+        add = TRUE, lwd = 2, col = "blue")
+
+  # Add vertical line at breakpoint
+  abline(v = breakpoint, lty = 2, col = "red")
+
+  # Display R-squared value
+  text(breakpoint, max(data$Height) * 0.9,
+       paste("R sq.=", round(tmp$r.squared, 4)), pos = 4, col = "blue")
+
+  # Return model summary
+  return(tmp)
+}
+
+
+# piecewise_regression(spruce.df, breakpoint = 18)
